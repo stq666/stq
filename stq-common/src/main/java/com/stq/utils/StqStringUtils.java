83df19d3a128c03238fdc940603ced6126558e54
stq <stqsht@163.com>
2016-05-30 17:45:17 +0800

    更改字符串工具类的名称StringUtils位StqStringUtils

--- stq-common/src/main/java/com/stq/utils/StqStringUtils.java
+++ stq-common/src/main/java/com/stq/utils/StqStringUtils.java
@@ -4,7 +4,8 @@ package com.stq.utils;
  * 字符串的工具类，此工具类不能包含任何的业务逻辑<br/>
  * Created by tianqiang.shao on 2016/5/30.
  */
-public class StringUtils {
+public class StqStringUtils {
 
-
+    public static void main(String[] args) {
+    }
 }
