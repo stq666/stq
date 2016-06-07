f7b89b7a7a22a8ee6bbe1e3ced916b85b25d894d
stq <stqsht@163.com>
2016-06-07 13:13:38 +0800

    登录和登出添加URL映射

--- stq-controller/src/main/java/com/stq/interceptors/SessionHandlerInterceptor.java
+++ stq-controller/src/main/java/com/stq/interceptors/SessionHandlerInterceptor.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * 作用：当用户用直接输入URL进行访问时，如果这个连接有session，则判断是否登录，如果没有登录则直接跳转到登陆页面<br/>
  *      如果这个连接没有session，则直接放行
- * Created by Administrator on 2016/6/3.
+ * Created by tianqiang.shao on 2016/6/3.
  */
 public class SessionHandlerInterceptor  implements HandlerInterceptor{
     @Override
