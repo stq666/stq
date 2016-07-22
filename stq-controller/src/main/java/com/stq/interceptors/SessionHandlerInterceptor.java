c52b3238ef2eb739301c860dc0f5c87af00be4ae
ubuntu <stqsht@163.com>
2016-07-22 16:20:20 +0800

    添加解析properties属性的工具类

--- stq-controller/src/main/java/com/stq/interceptors/SessionHandlerInterceptor.java
+++ stq-controller/src/main/java/com/stq/interceptors/SessionHandlerInterceptor.java
@@ -12,17 +12,14 @@ import javax.servlet.http.HttpServletResponse;
  * Created by tianqiang.shao on 2016/6/3.
  */
 public class SessionHandlerInterceptor  implements HandlerInterceptor{
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         return false;
     }
 
-    @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
     }
 
-    @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) throws Exception {
 
     }
