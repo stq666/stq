23868c9cb965c3a48f9085c5c776a5291f24ec11
ubuntu <stqsht@163.com>
2016-08-04 11:08:46 +0800

    提交

--- stq-controller/src/main/java/com/stq/interceptors/SessionHandlerInterceptor.java
+++ stq-controller/src/main/java/com/stq/interceptors/SessionHandlerInterceptor.java
@@ -13,11 +13,12 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class SessionHandlerInterceptor  implements HandlerInterceptor{
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return false;
+        System.out.println("开始前调用.....");
+        return true;
     }
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
+        System.out.println("开始后调用.....");
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) throws Exception {
